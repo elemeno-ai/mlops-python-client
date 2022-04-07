317cea2fab712030952128ad1894d10c974e3f5b
lucasbmiguel <lucas@elemeno.ai>
2022-04-06 19:07:21 -0700

    Bump version: 0.0.3 → 0.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, engineering@elemeno.ai'
 author = 'engineering@elemeno.ai'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
--- mlops_client/__init__.py
+++ mlops_client/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "__version__ = "__version__ = "0.0.3"""
+__version__ = "__version__ = "__version__ = "0.0.4"""
 """
 ### Elemeno MLOps Python SDK
 
