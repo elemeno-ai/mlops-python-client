a8e2d4a39b659c475b0f59f690f8aa1a0b87dca9
lucasbmiguel <lucas@elemeno.ai>
2022-04-06 23:09:56 -0700

    Bump version: 0.0.7 → 0.0.8

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, engineering@elemeno.ai'
 author = 'engineering@elemeno.ai'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.7'
+release = '0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
